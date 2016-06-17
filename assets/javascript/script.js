a1bf845996ef3d4e97be760e67f868a0576b1715
Gurjit Kaur <kgurjit@gmail.com>
2016-06-16 22:03:11 -0400

    fixed js and added current location code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,29 @@
-$(document).on('ready', function{
+$(document).on('ready', function(){
+    var map;
+
+    function getCurrentLocation(){
+        navigator.geolocation.getCurrentPosition(function(position){
+            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            map.setCenter(initialLocation);
+            var marker = new google.maps.Marker({
+                position: initialLocation,
+                map: map,
+                title: 'Current Location'
+            });
+        });
+    };
+
     function initialize() {
       var mapProp = {
         center: new google.maps.LatLng(40.69847032728747, -73.9514422416687),
         zoom:10,
         mapTypeId:google.maps.MapTypeId.ROADMAP
       };
-      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+      map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+      getCurrentLocation();
     }
-    google.maps.event.addDomListener(window, 'load', initialize
-    );
+
+    initialize();
 
     // var initialLocation;
     // var siberia = new google.maps.LatLng(60, 105);
