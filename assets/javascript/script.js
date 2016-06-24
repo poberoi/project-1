5fa33f80fe77611f9667466514b0563d633f1a72
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-06-24 11:27:23 -0400

    removed unwanted commented code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
   // Function to create and update event list
   function listEvents(zipcode){
     
-    // Create api url and set ajax and done request
+    // Create event api url and set ajax and done request
     var queryURL = 'https://crossorigin.me/http://api.eventful.com/json/events/search?app_key=P69H26fPKMJfCRcm&q=music&l=' + zipcode + '&within=10&units=miles&t=this+weekend';
 
     console.log(queryURL);
@@ -93,23 +93,30 @@ $(document).ready(function(){
     });
   };
 
-  // function getCurrentLocation(){
-  //   navigator.geolocation.getCurrentPosition(function(position){
-  //     console.log(position);
-  //     initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-  //     map.setCenter(initialLocation);
-  //     var marker = new google.maps.Marker({
-  //         position: initialLocation,
-  //         map: map,
-  //         title: 'Current Location'
-  //     });
-  //   });
-  // };
-  // getCurrentLocation();
-  //create initial list with default zipcode and place on map
+  // function to display parking areas
+  function paintParkingSpots(parkingData, eventPos){
+    if(parkingData.parking_listings && parkingData.parking_listings.length > 0) {
+      parkingData.parking_listings.forEach(function(p){
+        var marker = new google.maps.Marker({
+            map: map,
+            icon: parkingImage,
+            position: {lat: p.lat, lng: p.lng},
+            title: p.locaton_name
+          });
+        parkingMarkers.push(marker);
+      });
+      map.setZoom(14);
+      $("#msg").html('&nbsp;');
+    } else {
+      $("#msg").html('<h4>No parking around this area!</h4>');
+    }
+  }
+
+  //inital event list and map 
   listEvents('08859');
   initialize(40.4576,-74.3060);
   
+  //events after clicking submit button
   $('#submit').on('click', function(){
     $('#eventsDisplay').empty();
     $('#eventsHeading').empty();
@@ -119,35 +126,12 @@ $(document).ready(function(){
     return false;
   });
 
-  function paintParkingSpots(parkingData, eventPos){
-      if(parkingData.parking_listings && parkingData.parking_listings.length > 0) {
-          parkingData.parking_listings.forEach(function(p){
-              var marker = new google.maps.Marker({
-                  map: map,
-                  icon: parkingImage,
-                  position: {lat: p.lat, lng: p.lng},
-                  title: p.locaton_name
-                });
-              parkingMarkers.push(marker);
-          });
-          map.setZoom(14);
-          $("#msg").html('&nbsp;');
-      } else {
-          $("#msg").html('<h4>No parking around this area!</h4>');
-      }
-  }
-
+  //events after clicking event divs
   $(document).on('click', '.eventButtons', function(){
     console.log(this);
-    $('#thingsNearBy').empty();
-    $('#eventDescription').html($(this).data('desc'));
     var lat = $(this).data('lat');
     var lng = $(this).data('lng');
     initialize(lat,lng);
-    var params = {
-        'latitude': lat, 
-        'longitude': lng
-    };
     $("#msg").html('&nbsp;');
     var key = '8957d645f4fbb29b6265b2d55de30c5a';
     var url = 'https://crossorigin.me/http://api.parkwhiz.com/search?key=' + key + '&lat=' + lat + '&lng=' + lng;
@@ -157,202 +141,5 @@ $(document).ready(function(){
         paintParkingSpots(result, {lat: lat, lng: lng});
     });
   });
-
 });
 
-// $(document).on('ready', function(){
-//     var map;
-//     var markers = [];
-//     var parkingMarkers = [];
-
-//     var parkingImage = {
-//         url: './assets/images/parking.png',
-//         size: new google.maps.Size(32, 32),
-//         origin: new google.maps.Point(0, 0),
-//         anchor: new google.maps.Point(0, 32)
-//       };
-
-//     function getCurrentLocation(){
-//         navigator.geolocation.getCurrentPosition(function(position){
-//             initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-//             map.setCenter(initialLocation);
-//             var marker = new google.maps.Marker({
-//                 position: initialLocation,
-//                 map: map,
-//                 title: 'Current Location'
-//             });
-//         });
-//     };
-
-//     function initialize() {
-//       var mapProp = {
-//         center: new google.maps.LatLng(40.69847032728747, -73.9514422416687),
-//         zoom:10,
-//         mapTypeId:google.maps.MapTypeId.ROADMAP
-//       };
-//       map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-//       getCurrentLocation();
-//     }
-
-//     function clearMarkers(){
-//         for (var i = 0; i < markers.length; i++) {
-//             markers[i].setMap(null);
-//         }
-//         for (var i = 0; i < parkingMarkers.length; i++) {
-//             parkingMarkers[i].setMap(null);
-//         }
-
-//         markers = [];
-//         parkingMarkers = [];
-//     }
-
-//     initialize();
-
-//     function paintEvents(events){
-//         var resultsDiv = $("#results");
-//         $("#resultsWrapper").removeClass('hidden');
-//         if(events.length > 0) {
-//             resultsDiv.html('');
-//             clearMarkers();
-//             var eventData = [];
-//             var panToPos = null;
-
-//             for(x=0;x<events.length;x++){
-//                 var evt = $(events[x]);
-//                 var img = $(evt.children("medium")[0]).find("url").html();
-//                 console.log('Image: ' + img);
-//                 if(typeof img === 'undefined') {
-//                     img = './assets/images/placeholder1.png';
-//                 }
-//                 var evtDetails = {
-//                     title: evt.find("title").html(),
-//                     start: evt.find("start_time").html(),
-//                     end: evt.find("stop_time").html() ,
-//                     lat: evt.find("latitude").html(),
-//                     lng: evt.find("longitude").html(),
-//                     city: evt.find("city_name").html(),
-//                     region_name: evt.find("region_name").html(),
-//                     venue_address: evt.find("venue_address").html(),
-//                     image: img
-//                 };
-
-//                 eventData.push(evtDetails);
-//                 console.log(evtDetails);
-//                 var pos = {lat: parseFloat(evtDetails.lat), lng: parseFloat(evtDetails.lng)};
-//                 if(panToPos === null) {
-//                     panToPos = pos;
-//                 }
-//                 var marker = new google.maps.Marker({
-//                     map: map,
-//                     position: pos,
-//                     title: evtDetails.title
-//                   });
-//                 markers.push(marker);
-
-//                 var evtDiv = $("<div></div>").attr("class", "event-details");
-//                 var evtTitle =$("<span class='title'></span>").html(evtDetails.title + '&nbsp;'); 
-//                 var parkingLink = $("<a href='' class='parking-link'></a>").html('Parking');
-//                 parkingLink.attr('data-lat', evtDetails.lat);
-//                 parkingLink.attr('data-lng', evtDetails.lng);
-//                 parkingLink.attr('data-start', evtDetails.start);
-//                 parkingLink.attr('data-end', evtDetails.end);
-//                 evtTitle.append(parkingLink)
-//                 evtDiv.append(evtTitle);
-//                 resultsDiv.append(evtDiv);
-//             }
-//             map.setZoom(10);
-//             map.panTo(panToPos);
-//         } else {
-//             resultsDiv.html('No events found...');
-//         }
-//     }
-
-//     function paintParkingSpots(parkingData, eventPos){
-//         if(parkingData.parking_listings && parkingData.parking_listings.length > 0) {
-//             parkingData.parking_listings.forEach(function(p){
-//                 var marker = new google.maps.Marker({
-//                     map: map,
-//                     icon: parkingImage,
-//                     position: {lat: p.lat, lng: p.lng},
-//                     title: p.locaton_name
-//                   });
-//                 parkingMarkers.push(marker);
-//             });
-//             map.setZoom(16);
-//             map.panTo(eventPos);
-//         } else {
-//             alert('No parking spots around this event');
-//         }
-//     }
-
-//     $("#resultsWrapper").on("click", ".parking-link", function(e){
-//         e.preventDefault();
-//         var lat = $(this).data('lat');
-//         var lng = $(this).data('lng');
-//         var start = $(this).data('start');
-//         var end = $(this).data('end');
-//         if(end === '') {
-//             end = start;
-//         }
-
-//         var params = {
-//             'latitude': lat, 
-//             'longitude': lng,
-//             'start': new Date(start).getTime(),
-//             'end': new Date(end).getTime()
-//         };
-//         var key = '8957d645f4fbb29b6265b2d55de30c5a';
-//         var url = 'https://crossorigin.me/http://api.parkwhiz.com/search?key=' + key + '&lat=' + lat + '&lng=' + lng;
-//         $.ajax({
-//             url: url, method: 'GET'
-//         }).done(function(result){
-//             paintParkingSpots(result, {lat: lat, lng: lng});
-//         });
-//     });
-
-//     
-    
-
-//     // var initialLocation;
-//     // var siberia = new google.maps.LatLng(60, 105);
-//     // var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-//     // var browserSupportFlag =  new Boolean();
-
-//     // function initialize() {
-//     //   var myOptions = {
-//     //     zoom: 6,
-//     //     mapTypeId: google.maps.MapTypeId.ROADMAP
-//     //   };
-//     //   var map = new google.maps.Map(document.getElementById("map"), myOptions);
-
-//     //   // Try W3C Geolocation (Preferred)
-//     //   if(navigator.geolocation) {
-//     //     browserSupportFlag = true;
-//     //     navigator.geolocation.getCurrentPosition(function(position) {
-//     //       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-//     //       map.setCenter(initialLocation);
-//     //     }, function() {
-//     //       handleNoGeolocation(browserSupportFlag);
-//     //     });
-//     //   }
-//     //   // Browser doesn't support Geolocation
-//     //   else {
-//     //     browserSupportFlag = false;
-//     //     handleNoGeolocation(browserSupportFlag);
-//     //   }
-
-//     //   function handleNoGeolocation(errorFlag) {
-//     //     if (errorFlag == true) {
-//     //       alert("Geolocation service failed.");
-//     //       initialLocation = newyork;
-//     //     } else {
-//     //       alert("Your browser doesn't support geolocation. We've placed you in New York.");
-//     //       initialLocation = newyork;
-//     //     }
-//     //     map.setCenter(initialLocation);
-//     //   }
-//     // }
-
-
-
-// })
